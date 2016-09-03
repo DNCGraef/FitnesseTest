9b4ae6211139680f906c8ca92d6e7dd79c6df7e1
Daniel Graef <dncgraef@gmail.com>
2016-09-03 16:02:05 +0100

    Initial Commit

---
+++ FitnesseSandbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitnesseSandbox
+//
+//  Created by Daniel Graef on 03/09/2016.
+//  Copyright © 2016 dangraef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
